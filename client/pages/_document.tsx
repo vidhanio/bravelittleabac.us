bbca4dc41ad3ae5938cf2e95bc6ab893e30e5aae
Vidhan Bhatt <me@vidhan.io>
2022-02-28 18:19:20 -0500

    feat: ✨ move font location

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -3,12 +3,6 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Gaegu:wght@300;400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
       <body
         className={
           "flex flex-col items-center bg-stone-100 font-handwritten text-stone-900 dark:bg-stone-900 dark:text-stone-100"
